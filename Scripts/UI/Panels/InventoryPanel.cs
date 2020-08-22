737d6326cd4e830762a544327922cac578f4ecf9
Quentin Gendre <swynfel@gmail.com>
2020-08-22 18:36:07 +0900

    Add breakpoint for inventory to avoid horizontal overflow

--- Scripts/UI/Panels/InventoryPanel.cs
+++ Scripts/UI/Panels/InventoryPanel.cs
@@ -7,6 +7,7 @@ namespace UI {
         public override void _Ready() {
             container = GetNode<GridContainer>("Grid");
             Connect("visibility_changed", this, nameof(Refresh));
+            container.Connect("resized", this, nameof(on_Resized));
             Refresh();
         }
 
@@ -15,6 +16,12 @@ namespace UI {
             foreach (Item item in Game.data.inventory.items) {
                 container.AddChild(Visual.Tables.ItemTable.Create(item));
             }
+        }
+
+        private const int MIN_COLUMN_SIZE = 132;
+
+        private void on_Resized() {
+            container.Columns = (int) (container.RectSize.x / MIN_COLUMN_SIZE);
         }
     }
 }
